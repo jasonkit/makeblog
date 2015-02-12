11252190b693084ae6e1f97a3a6fea0468b2899a
jasonkit <jasonkit321@gmail.com>
2015-02-12 16:13:05 +0100

    seperate the template related logic

--- makeentry.js
+++ makeentry.js
@@ -1,6 +1,5 @@
 "use strict";
 var MarkedMetaData = require("marked-metadata-with-options");
-var Handlebars = require("Handlebars");
 var getopt = require("node-getopt");
 var fs = require("fs");
 var path = require("path");
@@ -13,64 +12,33 @@ var options = getopt.create([
         ["h", "help",           "display this help."]
         ]).bindHelp().parseSystem().options;
 
-// setup Handlebars
-var template_path = options.template;
+// Template Engine Config
+var template = require([options.template,"template.js"].join(path.sep))(options, require("Handlebars"));
+template.init();
 
-Handlebars.registerPartial("header", fs.readFileSync([__dirname,template_path,"partials","header.hbt"].join(path.sep)).toString());
-Handlebars.registerPartial("footer", fs.readFileSync([__dirname,template_path,"partials","footer.hbt"].join(path.sep)).toString());
-Handlebars.registerHelper("formatDate", function(date, lang){
-    var date = new Date(date);
-
-    if (lang) {
-        return date.getFullYear()+"年 "+(date.getMonth()+1)+"月 "+date.getDate()+"日";
-    }else {
-        var month = ["January", "Febuary", "March", "April", "May", "June",
-                     "July", "August", "September", "October", "November", "December"];
-
-        return month[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();        
-    }
-});
-
+// marked Config
 var md = new MarkedMetaData(options.input);
+md.defineTokens("---", "---");
 md.setOptions({
-    highlight: function (code) {
-        return highlight.highlight("c",code).value;
+    highlight: function (code, lang) {
+        return highlight.highlight(lang, code).value;
     }
 });
-md.defineTokens("---", "---");
 
 try {
     var context = md.metadata();
     context.contents = md.markdown();
 } catch(err) {
     console.log("Bad markdown file or metadata: ", options.input); 
-    process.exit(-1);
+    process.exit(1);
 }
 
-context.short_title = path.basename(options.input, ".md");
-
-if (context.template === undefined) {
-    switch (path.basename(path.dirname(options.input))) {
-        case "pages":
-            context.template = "page.hbt";
-            break;
-        case "posts":
-            context.template = "post.hbt";
-            break;
-        default:
-            console.log("template not found for", options.input);
-            process.exit(-1);
-    }
-}
-
-var template = Handlebars.compile(fs.readFileSync([__dirname,template_path,context.template].join(path.sep)).toString());
-var html = template(context);
 try {
     if (!fs.existsSync(path.dirname(options.output))) {
         fs.mkdirSync(path.dirname(options.output));
     }
-    fs.writeFileSync(options.output, html);
-}catch(err) {
+    fs.writeFileSync(options.output, template.apply(context));
+} catch(err) {
     console.log("Cannot write output to", options.output);
-    process.exit(-1);
+    process.exit(2);
 }
